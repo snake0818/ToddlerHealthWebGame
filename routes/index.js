000b1cf234fb50dbdf295e3e2e440513192edc1b
snake0818 <cs081807@gmail.com>
2024-03-17 13:39:51 +0800

    add pinyin and tone

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+const toneRegex = /[ˋˇˊ]/;
+const word = [
+  { original: '食物分類', pinyin: ['ㄕˊ', 'ㄨˋ', 'ㄈㄣ', 'ㄌㄟˋ'] },
+  { original: '食物的家', pinyin: ['ㄕˊ', 'ㄨˋ', '˙ㄉㄜ', 'ㄐㄧㄚ'] },
+  { original: '念謠配對', pinyin: ['ㄋㄧㄢˋ', 'ㄧㄠˊ', 'ㄆㄟˋ', 'ㄉㄨㄟˋ'] },
+  { original: '彩虹蔬果的家', pinyin: ['ㄘㄞˇ', 'ㄏㄨㄥˊ', 'ㄕㄨ', 'ㄍㄨㄛˇ', '˙ㄉㄜ', 'ㄐㄧㄚ'] },
+  { original: '蔬果配對身體', pinyin: ['ㄕㄨ', 'ㄍㄨㄛˇ', 'ㄆㄟˋ', 'ㄉㄨㄟˋ', 'ㄕㄣ', 'ㄊㄧˇ'] },
+  { original: '我的餐盤', pinyin: ['ㄨㄛˇ', '˙ㄉㄜ', 'ㄘㄢ', 'ㄆㄢˊ'] },
+];
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: '數位遊戲' });
+  res.render('index', { title: '數位遊戲', word, toneRegex });
 });
 /* GET test page. */
-router.get('/game1', function (req, res, next) { res.render('game', { game: 'game1', title: '金字塔遊戲之食物分類' }); });
-router.get('/game2', function (req, res, next) { res.render('game', { game: 'game2', title: '金字塔遊戲之找食物的家' }); });
-router.get('/game3', function (req, res, next) { res.render('game', { game: 'game3', title: '念謠配對圖卡' }); });
-router.get('/game4', function (req, res, next) { res.render('game', { game: 'game4', title: '彩虹蔬果的家' }); });
-router.get('/game5', function (req, res, next) { res.render('game', { game: 'game5', title: '蔬果顏色身體配對遊戲' }); });
-router.get('/game6', function (req, res, next) { res.render('game', { game: 'game6', title: '我的餐盤遊戲' }); });
+router.get('/game1', function (req, res, next) { res.render('game', { game: 'game1', title: word.original }); });
+router.get('/game2', function (req, res, next) { res.render('game', { game: 'game2', title: word.original }); });
+router.get('/game3', function (req, res, next) { res.render('game', { game: 'game3', title: word.original }); });
+router.get('/game4', function (req, res, next) { res.render('game', { game: 'game4', title: word.original }); });
+router.get('/game5', function (req, res, next) { res.render('game', { game: 'game5', title: word.original }); });
+router.get('/game6', function (req, res, next) { res.render('game', { game: 'game6', title: word.original }); });
 
 module.exports = router;
